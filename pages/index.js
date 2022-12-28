87123a02d7f7efad4c8b5fc9b834ae05b6cdf145
a1a2a3a4a5 <linub7@gmail.com>
2022-12-28 12:40:34 +0330

    switch to sass - some initial global css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
@@ -12,6 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1 className={styles.red}>Welcome</h1>
     </>
   );
 }
