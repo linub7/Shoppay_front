5fa3247ab1d008b811caaec53d4c3f3f8b265140
a1a2a3a4a5 <linub7@gmail.com>
2023-01-04 17:40:12 +0330

    make responsive home flashDeals

--- components/home/flash-deals/swiper/index.js
+++ components/home/flash-deals/swiper/index.js
@@ -14,10 +14,28 @@ import FlashDealsSwiperOffersCard from './offers-card';
 const FlashDealsSwiper = () => {
   return (
     <Swiper
-      slidesPerView={6}
-      spaceBetween={20}
-      modules={[]}
+      slidesPerView={1}
+      spaceBetween={10}
+      navigation={true}
+      modules={[Navigation]}
       className="flashDeals__swiper"
+      breakpoints={{
+        450: {
+          slidesPerView: 2,
+        },
+        630: {
+          slidesPerView: 3,
+        },
+        920: {
+          slidesPerView: 4,
+        },
+        1232: {
+          slidesPerView: 5,
+        },
+        1520: {
+          slidesPerView: 6,
+        },
+      }}
     >
       <div className={styles.flashDeals__list}>
         {flashDealsArray.map((deal, i) => (
