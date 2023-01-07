475b46ed4e83fb8e0104fb61fc803b49b2c9e16d
a1a2a3a4a5 <linub7@gmail.com>
2023-01-07 11:56:36 +0330

    product images main swiper -> done

--- components/single-product/index.js
+++ components/single-product/index.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
+import SingleProductComponentImagesSwiper from './images-swiper';
 import SingleProductComponentPath from './path';
 import styles from './styles.module.scss';
 
 const SingleProductPageComponent = ({ product }) => {
+  const [activeImg, setActiveImg] = useState('');
   return (
     <div className={styles.product}>
       <div className={styles.container}>
         <SingleProductComponentPath product={product} />
+        <div className={styles.product__main}>
+          <SingleProductComponentImagesSwiper
+            images={product?.images}
+            activeImg={activeImg}
+            setActiveImg={setActiveImg}
+          />
+        </div>
       </div>
     </div>
   );
