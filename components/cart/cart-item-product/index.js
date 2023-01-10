6b95704d47986db949f1be3deb2634635b0daacb
a1a2a3a4a5 <linub7@gmail.com>
2023-01-10 12:20:27 +0330

    cart item product & style -> done

---
+++ components/cart/cart-item-product/index.js
@@ -0,0 +1,77 @@
+/* eslint-disable @next/next/no-img-element */
+import {
+  IoArrowForwardOutline,
+  IoHeartOutline,
+  IoTrashOutline,
+} from 'react-icons/io5';
+import styles from './styles.module.scss';
+
+const CartPageComponentCartItemProduct = ({ product }) => {
+  return (
+    <div className={`${styles.cart} ${styles.product}`}>
+      {product?.quantity < 1 && <div className={styles.blue}></div>}
+      <div className={styles.product__header}>
+        <img src="/images/store.png" alt="image" />
+        John official store
+      </div>
+      <div className={styles.product__image}>
+        <div className={styles.checkbox}></div>
+        <img src={product?.images[0].url} alt="product image" />
+        <div className={styles.col}>
+          <div className={styles.grid}>
+            <h1>
+              {product?.name?.length > 20
+                ? `${product?.name.substring(0, 30)}`
+                : product?.name}
+            </h1>
+            <div style={{ zIndex: '2' }}>
+              <IoHeartOutline />
+            </div>
+            <div style={{ zIndex: '2' }}>
+              <IoTrashOutline />
+            </div>
+          </div>
+          <div className={styles.product__style}>
+            <img src={product?.color?.image} alt="color image" />
+            {product?.size && <span>{product?.size}</span>}
+            {product?.price && <span>{product?.price?.toFixed(2)}</span>}
+            <IoArrowForwardOutline />
+          </div>
+          <div className={styles.product__priceQty}>
+            <div className={styles.product__priceQty_price}>
+              <span className={styles.price}>
+                USD {(product?.price * product?.qty).toFixed(2)}$
+              </span>
+              {product?.price !== product?.priceBefore && (
+                <span className={styles.priceBefore}>
+                  USD {product?.priceBefore}$
+                </span>
+              )}
+              {product?.discount > 0 && (
+                <span className={styles.discount}>-{product?.discount}%</span>
+              )}
+            </div>
+            <div className={styles.product__priceQty_qty}>
+              <button disabled={product?.qty < 2}>-</button>
+              <span>{product?.qty}</span>
+              <button disabled={product?.qty === product?.quantity}>+</button>
+            </div>
+          </div>
+          <div className={styles.product__shipping}>
+            {product?.shipping
+              ? `+${product?.shipping}$ Shipping Fee`
+              : 'Free Shipping'}
+          </div>
+          {product?.quantity < 1 && (
+            <div className={styles.notAvailable}>
+              This product is out of stock for now, Add it to your wishlist it
+              may get restocked.
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartPageComponentCartItemProduct;
--- components/cart/index.js
+++ components/cart/index.js
@@ -1,15 +1,26 @@
+import { useSelector } from 'react-redux';
+import CartPageComponentCartItemProduct from './cart-item-product';
 import CartPageComponentEmptyCart from './empty';
 import CartPageComponentHeader from './header';
 import styles from './styles.module.scss';
 
 const CartPageComponent = () => {
-  const cart = [];
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <>
-      <CartPageComponentHeader />
       <div className={styles.cart}>
-        {cart?.length > 1 ? (
-          <div className={styles.cart__container}></div>
+        <div className={styles.cart__header}>
+          <CartPageComponentHeader />
+        </div>
+        {cartItems?.length >= 1 ? (
+          <div className={styles.cart__container}>
+            <div className={styles.cart__products}>
+              {cartItems?.map((item, index) => (
+                <CartPageComponentCartItemProduct key={index} product={item} />
+              ))}
+            </div>
+          </div>
         ) : (
           <CartPageComponentEmptyCart />
         )}
